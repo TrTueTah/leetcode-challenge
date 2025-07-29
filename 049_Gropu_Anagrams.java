887aedcc41306d12f437b795159306d75f636070
Tanh Tran <119776880+TrTueTah@users.noreply.github.com>
2025-07-29 13:09:39 +0700

    29/07/2025

--- 049_Gropu_Anagrams.java
+++ 049_Gropu_Anagrams.java
@@ -6,15 +6,29 @@ import java.util.Map;
 
 class Solution {
     // Solution 1: Sorting Method
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     Map<String, List<String>> res = new HashMap<>();
+
+    //     for(String s : strs) {
+    //         char[] charArray = s.toCharArray();
+    //         Arrays.sort(charArray);
+    //         String sortedS = new String(charArray);
+    //         res.putIfAbsent(sortedS, new ArrayList<>());
+    //         res.get(sortedS).add(s);
+    //     }
+    //     return new ArrayList<>(res.values());
+    // }
+    // Solution 2: Hash Table
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res = new HashMap<>();
-
         for(String s : strs) {
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String sortedS = new String(charArray);
-            res.putIfAbsent(sortedS, new ArrayList<>());
-            res.get(sortedS).add(s);
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
         }
         return new ArrayList<>(res.values());
     }
